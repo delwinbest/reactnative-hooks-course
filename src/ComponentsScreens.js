1acb15e7bbb51e4ea3b01fca5d8622b61b406ae7
Delwin Best <mail@delwinbest.com>
2023-03-27 14:20:18 -0700

    basics

--- src/ComponentsScreens.js
+++ src/ComponentsScreens.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-  const greeting = <Text>hellooooo</Text>;
+  const name = 'delwin';
   return (
     <View>
-      <Text style={styles.textStyle}>This is the components screen</Text>
-      {greeting}
+      <Text style={styles.textStyle}>Getting start with React Native</Text>
+      <Text style={styles.subHeaderStyle}>My name is {name}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30,
+    fontSize: 45,
+  },
+  subHeaderStyle: {
+    fontSize: 20,
   },
 });
 
