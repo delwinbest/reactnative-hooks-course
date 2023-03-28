717de7eab68af6e8eefd57a97efedfdd4c3b4afe
Delwin Best <mail@delwinbest.com>
2023-03-28 13:08:22 -0700

    Add button

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,26 +3,36 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
   const friends = [
-    { name: 'Friend #1' },
-    { name: 'Friend #2' },
-    { name: 'Friend #3' },
-    { name: 'Friend #4' },
-    { name: 'Friend #5' },
-    { name: 'Friend #6' },
-    { name: 'Friend #7' },
-    { name: 'Friend #8' },
-    { name: 'Friend #9' },
+    { name: 'Friend #1', age: 10 },
+    { name: 'Friend #2', age: 20 },
+    { name: 'Friend #3', age: 30 },
+    { name: 'Friend #4', age: 40 },
+    { name: 'Friend #5', age: 50 },
+    { name: 'Friend #6', age: 60 },
+    { name: 'Friend #7', age: 70 },
+    { name: 'Friend #8', age: 80 },
+    { name: 'Friend #9', age: 90 },
   ];
   return (
     <FlatList
+      horizontal={false}
+      keyExtractor={(item) => item.name}
       data={friends}
-      renderItem={({ item }) => {
-        return <Text>{item.name}</Text>;
+      renderItem={({ item, index }) => {
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} - Age: {item.age}
+          </Text>
+        );
       }}
     ></FlatList>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50,
+  },
+});
 
 export default ListScreen;
