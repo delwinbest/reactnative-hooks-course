58cd1c4a1578592ad1deea4805585ed2b107c774
Delwin Best <mail@delwinbest.com>
2023-03-28 19:53:49 -0700

    useState

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import ComponentsScreen from './src/ComponentsScreens';
+import CounterScreen from './src/screens/CounterScreen';
 import HomeScreen from './src/screens/HomeScreen';
+import ImageScreen from './src/screens/ImageScreen';
 import ListScreen from './src/screens/ListScreen';
 
 const navigator = createStackNavigator(
@@ -9,6 +11,8 @@ const navigator = createStackNavigator(
     Home: HomeScreen,
     Components: ComponentsScreen,
     List: ListScreen,
+    Image: ImageScreen,
+    Counter: CounterScreen,
   },
   {
     initialRouteName: 'Home',
