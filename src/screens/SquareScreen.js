b3fe22d904f78dd8422ebd990a41416455f951a0
Delwin Best <mail@delwinbest.com>
2023-03-31 15:25:56 -0700

    convert if to ternary expression

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -9,14 +9,17 @@ const reducer = (state, action) => {
 
   switch (action.colorToChange) {
     case 'red':
-      if (state.red + action.amount < 0 || state.red + action.amount > 255) return { ...state };
-      return { ...state, red: state.red + action.amount };
+      return state.red + action.amount < 0 || state.red + action.amount > 255
+        ? state
+        : { ...state, red: state.red + action.amount };
     case 'green':
-      if (state.green + action.amount < 0 || state.green + action.amount > 255) return { ...state };
-      return { ...state, green: state.green + action.amount };
+      return state.green + action.amount < 0 || state.green + action.amount > 255
+        ? state
+        : { ...state, green: state.green + action.amount };
     case 'blue':
-      if (state.blue + action.amount < 0 || state.blue + action.amount > 255) return { ...state };
-      return { ...state, blue: state.blue + action.amount };
+      return state.blue + action.amount < 0 || state.blue + action.amount > 255
+        ? state
+        : { ...state, blue: state.blue + action.amount };
     default:
       return;
   }
